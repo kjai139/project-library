d821c4cf3f96ebbd3c2afe7e3c7dc525d0dc0f77
Simon Y <therealkjai139@gmail.com>
2022-07-01 15:44:52 -0400

    card items create

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 let myLibrary = []
+let cardContainer = document.querySelector('.cardContainer')
 
-function book(title) {
+function book(title, cover, read) {
     this.title = title
+    this.cover = cover
+    this.read = read
 }
 
 let addBookToLibrary = (...book) => {
@@ -11,14 +14,65 @@ let addBookToLibrary = (...book) => {
     
 }
 
+//function to make cards
 
-let harryPotter = new book('Harry Potter')
-let overLord = new book('Overlord')
+let createCards = () => {
+    for (let x = 0; x < myLibrary.length; x++) {
+        console.log(myLibrary[x][x].cover)
+        
+        let newCard = document.createElement('div')
+        newCard.classList.add('card')
+        newCard.setAttribute('id', `card${x}`)
+
+        let newImage = document.createElement('img')
+        newImage.classList.add('cardCover')
+        if (myLibrary[x][x].cover != null) {
+            newImage.setAttribute('src', `${myLibrary[x][x].cover}`)
+        } else if (myLibrary[x][x].cover == null) {
+            newImage.setAttribute('src', 'images/covers/nocover.jpeg')
+        } 
+        newCard.appendChild(newImage)
+
+        let newTitle = document.createElement('p')
+        newTitle.textContent = `${myLibrary[x][x].title}`
+        newCard.appendChild(newTitle)
+
+        //let checkboxDiv = document.createElement('div')
+        //checkboxDiv.classList.add('checkboxDiv')
+        //newCard.appendChild(checkboxDiv)
+
+        let newCheckboxLabel = document.createElement('label')
+        newCheckboxLabel.classList.add('labels')
+        newCheckboxLabel.textContent = 'Read'
+        newCard.appendChild(newCheckboxLabel)
+
+
+        let newCheckbox = document.createElement('input')
+        newCheckbox.classList.add('checkbox')
+        newCheckbox.setAttribute('id', `checkbox${x}`)
+        newCheckbox.setAttribute('type', 'checkbox')
+        newCheckboxLabel.appendChild(newCheckbox)
+
+        
+
+        cardContainer.appendChild(newCard)
+
+    }
+}
+
+
+let harryPotter = new book('Mushoku Tensei', 'images/covers/mushoku1.jpg')
+let overLord = new book('Overlord', 'images/covers/overlord1.jpg')
 
 addBookToLibrary(harryPotter, overLord)
+
+createCards()
 
 
 
 myLibrary.forEach(element => {
     console.log(element)
+    console.log(element[0].title)
+    
+    
 });
